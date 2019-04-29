fefa7dfc5290ae412e35a87192d85ed264a26168
Simon DENEL <sdenel@smartwavesa.com>
2019-04-29 22:49:56 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Usage
+You must have a Windscribe account to use this image. The free offer includes 10Go/month: [Create an account (affiliated link)](https://windscribe.com/?affid=b61tse8h)
+```bash
+docker run --cap-add=NET_ADMIN --device=/dev/net/tun -ti sdenel/docker-windscribe
+```
+# See also...
+* https://cloud.docker.com/repository/docker/sdenel/docker-windscribe/general
---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+#
+# Install Pandoc
+#
+apt update
+apt install -y wget
+wget https://github.com/jgm/pandoc/releases/download/2.7.2/pandoc-2.7.2-1-amd64.deb
+dpkg -i pandoc-2.7.2-1-amd64.deb
+
+#
+# Python tools: Jinja2 + ruamel.yaml
+#
+apt install python3-pip
+pip3 install jinja2 ruamel.yaml
+
+#
+# Cleaning the cache to limit the size of the image
+#
+apt clean
